9005452a87f13bd5531f361b22bc17f12ffbed4d
Gundam2887 <657527502@qq.com>
2019-10-14 11:29:53 +0800

    work5

---
+++ work5/work5.js
@@ -0,0 +1,39 @@
+str1 ='<table border="1">'
+for(let i=1;i<10;++i){
+    str1 +='<tr>'
+    for(var j=1 ;j<=i;++j){
+        //;拼接单元格
+        str1 += '<td>&nbsp;</td>'
+    }
+    str1 +='</tr>'
+}
+str1 +='</table>'
+//设置div的html文档内容
+document.getElementById('table1').innerHTML=str1
+
+str2 ='<table border="1">'
+for(let i=9;i>0;--i){
+    str2 +='<tr>'
+    for(var j=i ;j>0;--j){
+        //;拼接单元格
+        str2 += '<td>&nbsp;</td>'
+    }
+    str2 +='</tr>'
+}
+str2 +='</table>'
+//设置div的html文档内容
+document.getElementById('table2').innerHTML=str2
+ 
+str3 ='<table border="1">'
+for(let i=9;i>0;--i){
+    str3 +='<tr>'
+    for(var j=i ;j>0;--j){
+        //;拼接单元格
+        str3 += '<td>'+ j + '*' + i + '=' + ( j * i )+'</td>'
+    }
+    str3 +='</tr>'
+}
+str3 +='</table>'
+//设置div的html文档内容
+document.getElementById('table3').innerHTML=str3
+ 
