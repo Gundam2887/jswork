56c03bf9c86baf23fca421c8014d220094d396cb
Gundam2887 <657527502@qq.com>
2019-10-17 10:01:00 +0800

    work6

---
+++ work6/pi.js
@@ -0,0 +1,24 @@
+function calculate(){
+    let num =document.getElementById('pi').Value
+    num=parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('pi').Value=pi(num)
+    }
+}
+function factorial(n){
+    let sum=1
+    for(let i=1;i<=n;++i)
+    sum * i
+}
+return sum
+
+function pi(num){
+    let sum =0
+    for (let i=0;i<=num;++i){
+        let dividend = factorial()
+        let dividend=oddFactorial()
+        sum
+    }
+    return sum*2
+}
+calculate()
