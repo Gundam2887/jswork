1d33d59fa7e857c615228a87c2246e34bcf81e04
Gundam <657527502@qq.com>
2019-10-18 09:36:51 +0800

    work6 2.0

--- work6/pi.js
+++ work6/pi.js
@@ -1,24 +1,37 @@
+//公式：派/2=n！/（2n+1）！！
 function calculate(){
-    let num =document.getElementById('pi').Value
-    num=parseInt(num)&&Number(num)
+    let num = document.getElementById('num').value
+    num = parseInt(num)&&Number(num)
     if(!isNaN(num)){
-        document.getElementById('pi').Value=pi(num)
+        document.getElementById('pi').value = pi(num)
     }
 }
+
+//分子
 function factorial(n){
-    let sum=1
-    for(let i=1;i<=n;++i)
-    sum * i
+    let sum = 1
+    for(let i=1;i<=n;++i){
+      sum *=i
+    }
+    return sum
 }
-return sum
+
+//分母
+function oddFactorial(n){
+    let sum = 1
+    for(let i=1;i<=n;++i){
+      sum *=(2*i+1)
+    }
+    return sum
+}
 
 function pi(num){
-    let sum =0
-    for (let i=0;i<=num;++i){
-        let dividend = factorial()
-        let dividend=oddFactorial()
-        sum
+    let sum = 0
+    for(let i=0;i<=num;++i){
+      let dividend = factorial(i)
+      let divisor = oddFactorial(i)
+      sum +=(dividend/divisor)
     }
-    return sum*2
+    return sum * 2
 }
 calculate()
