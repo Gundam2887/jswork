e7e13812d8dd95548b432c662bb63c760711a7a5
gundam2887 <gundam2887>
2019-12-19 10:04:34 +0800

    work17

---
+++ work17/index.js
@@ -0,0 +1,46 @@
+var input = document.getElementsByTagName('input');
+for(var i = 0; i < inputs.length - 1; ++i){
+    input[i].onblur = inputBlur;
+}
+function inputBlur() {
+    var name = this.name;
+    var val = this.value;
+    var tips = this.placeholder;
+    var tips_obj = this.parentNode.nextElemetSibling;
+    val = val.trim();
+    if (!val){
+        error(tips_obj, '输入框不能为空');
+        return false;
+    }
+    var reg_msg = getRegMsg(name,tips);
+    if(reg_msg['reg'].test(val)){
+        success(tips_obj,reg_msg['msg']['success']);
+    }else{
+        error(tips_obj,reg_msg['msg']['error']);
+    }
+}
+
+function getRegMsg(name,tips){
+    var reg=msg='';
+    switch(name){
+        case 'username':
+            reg=/^[a-zA-Z]{4,12}$/;
+            msg = {'success':'用户名输入正确','error':tips};
+            break;
+        case 'pwd':
+            reg = /^\w{6,20}$/;
+            msg = {'success':'密码输入正确','errpr':tips};
+            break;
+        case 'repwd':
+            var con = document.getElementsByTagName('input')[1].value;
+            reg = RegExp("^"+com+"$");
+            msg = {'success':'两次密码输入正确', 'error':'两次密码不一致'}；
+            break;
+        case 'tel':
+            reg = /^1[134578]\d{9}$/;
+            msg = {'success' : '手机号码输入正确', 'error':tips};
+            break;
+        case 'email':
+            reg=/^()/
+    }
+}
